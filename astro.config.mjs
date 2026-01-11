3f180c78e780e04eb2a7ee5076008452851906b4
Fabian Mäkilä <contact@fabianadrian.fi>
2026-01-11 20:28:57 +0200

    update for new domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://mc.fabianadrian.fi',
+  site: 'https://fabicraft.net',
 	integrations: [
 		starlight({
 			title: 'FabiCraft',
